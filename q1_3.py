aca8f7814c2991fb2c2dcde83a55601fc5dad805
Vasvi Agarwal <100332832+vagarwal43@users.noreply.github.com>
2025-01-27 20:34:02 -0500

    Add files via upload

---
+++ q1_3.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+from utils import get_args
+
+import torch
+
+from networks import Discriminator, Generator
+import torch.nn.functional as F
+from train import train_model
+
+
+def compute_discriminator_loss(
+    discrim_real, discrim_fake, discrim_interp, interp, lamb
+):
+    ##################################################################
+    # TODO 1.3: Implement GAN loss for discriminator.
+    # Do not use discrim_interp, interp, lamb. They are placeholders
+    # for Q1.5.
+    ##################################################################
+    real_labels = torch.ones_like(discrim_real)  
+    fake_labels = torch.zeros_like(discrim_fake)  
+
+    loss_real = F.binary_cross_entropy_with_logits(discrim_real, real_labels)
+    loss_fake = F.binary_cross_entropy_with_logits(discrim_fake, fake_labels)
+    
+    loss = loss_real + loss_fake
+    ##################################################################
+    #                          END OF YOUR CODE                      #
+    ##################################################################
+    return loss
+
+
+def compute_generator_loss(discrim_fake):
+    ##################################################################
+    # TODO 1.3: Implement GAN loss for the generator.
+    ##################################################################
+    labels_fake = torch.ones_like(discrim_fake)
+    loss = F.binary_cross_entropy_with_logits(discrim_fake, labels_fake)
+    ##################################################################
+    #                          END OF YOUR CODE                      #
+    ##################################################################
+    return loss
+
+
+if __name__ == "__main__":
+    args = get_args()
+    gen = Generator().cuda()
+    disc = Discriminator().cuda()
+    prefix = "data_gan/"
+    os.makedirs(prefix, exist_ok=True)
+
+    train_model(
+        gen,
+        disc,
+        num_iterations=int(3e4),
+        batch_size=256,
+        prefix=prefix,
+        gen_loss_fn=compute_generator_loss,
+        disc_loss_fn=compute_discriminator_loss,
+        log_period=1000,
+        amp_enabled=not args.disable_amp,
+    )
